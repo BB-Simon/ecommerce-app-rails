14b0efd7893a2cbff8cc7eac4880af5e3c75682b
BB-Simon <simon.chowdery@gmail.com>
2024-07-04 02:41:10 +0300

    Add models

---
+++ db/migrate/20240703233854_create_payment_methods.rb
@@ -0,0 +1,12 @@
+class CreatePaymentMethods < ActiveRecord::Migration[7.1]
+  def change
+    create_table :payment_methods do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :card_number
+      t.string :expiration_date
+      t.string :cardholder_name
+
+      t.timestamps
+    end
+  end
+end
